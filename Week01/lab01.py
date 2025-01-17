bca16589c9ac49e8b33caa0522e0cb6b82ecab8b
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-01-17 14:35:17 -0500

    Writing coding questions inside lab01.py

---
+++ Week01/lab01.py
@@ -0,0 +1,22 @@
+# Sample Coding Questions 01 Week 01
+# Student Name: Breno Lopes Mafra
+# Student ID: 101485572
+
+# Defining variables
+arr = [1, 4, 7, 9]
+
+# Order of operations
+a = 1
+b = 2
+c = 3
+d = 4
+e = ((a - ((b ** c) // d)) + (a % c))
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common functions
+userAge = int(input("Enter your age: "))
+userAge += 22
+print(f"Now showing the shop items filtered by age: {userAge}")
